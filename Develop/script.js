3a17fe5ead64246128bf6b6f2fe111ac5c2abaef
carl-33 <koepkecj@gmail.com>
2020-09-24 21:27:26 -0500

    removed excess code, and added descriptive comments

--- Develop/script.js
+++ Develop/script.js
@@ -1,50 +1,29 @@
-// Assignment Code
 var generateBtn = document.querySelector("#generate");
-
+// creating character arrays
 var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 var lowerCase = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 var numeric = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 var symbols = ["~","!","@","#","$","%","&","-","+","=", "_"];
-
+// Creating an empty array to concat arrays to.
 var passwordArray = [];
-var passwordArray = [];
-
-// Write password to the #password input
-// function writePassword() {
-//   var password = generatePassword();
-//   var passwordText = document.querySelector("#password");
-
-//   passwordText.value = password;
-// };
-
-// Add event listener to generate button
-generateBtn.addEventListener("click", function () {
-  
+// password generator
+generateBtn.addEventListener("click", myFunction); 
+function myFunction() {
   var Pword = "";
   var password = generatePassword();
-  // var passwordText = document.querySelector("#password");
-
-  // passwordText.value = password;
-// });
-
-
-
-
-
   function generatePassword (){
-
+    // password length prompt
     var Length = parseInt(prompt ("How long would you like your password to be? It can be between 8 and 128 characters long."));
-    while (Length > 128 || Length < 8) {
+    // If a number lower than 8 or higher than 128 is answered the user is prompted again for a password length.
+    while (Length > 128 || Length < 8 ) {
       Length = parseInt(prompt ("Whoops, your password has to be between 8 and 128 characters.  How long would you like it to be?"));
-
     }
-
-    var confirmUpper = confirm("Whould you like to include upper case letter?");
-    var confirmLower = confirm("Would you like to include lower case letter?");
-    var confirmSymbols = confirm("Would you like to include special characters?");
-    var confirmNumbers = confirm("Would you like to include numbers?");
-
-    
+    // character confirms
+    var confirmUpper = confirm("Would you like to include upper case letter? OK for yes, Cancel for no.");
+    var confirmLower = confirm("Would you like to include lower case letter? OK for yes, Cancel for no.");
+    var confirmSymbols = confirm("Would you like to include special characters? OK for yes, Cancel for no.");
+    var confirmNumbers = confirm("Would you like to include numbers? OK for yes, Cancel for no.");
+    // adding the coresponding arrays to passwordArray, the formerlly empty array.
     if (confirmUpper) {
       passwordArray = passwordArray.concat(upperCase)
     }
@@ -57,31 +36,14 @@ generateBtn.addEventListener("click", function () {
     if (confirmNumbers) {
       passwordArray = passwordArray.concat(numeric)
     }
-
-
-
-console.log(passwordArray);
-
-for (let i = 0; i < passwordArray.length; i++) {
-  console.log(passwordArray[i]);
-  
-}
-   
-
-  for (let i = 0; i < Length; i++) {
-    var variables = Math.floor(Math.random()*passwordArray.length);
-    Pword += passwordArray[variables];
-    console.log(Pword);
-
+  // generating the random password
+    for (let i = 0; i < Length; i++) {
+      var variables = Math.floor(Math.random()*passwordArray.length);
+      Pword += passwordArray[variables];
+    }
   }
-console.log(Pword);
-
-}
-
-console.log(Pword);
-
+  // adding the password to the HTML page for the ID=password
   var passwordText = document.querySelector("#password");
-
   passwordText.value = Pword;
-});
+};
 
